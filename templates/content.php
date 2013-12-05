a75a2fe5d0e17f2d0551d1a09f0f7ef99d77c812
sladiri <sladjanri@live.com>
2013-12-05 23:44:56 +0100

    readd flexbox html file, fix indent in content.php template

--- templates/content.php
+++ templates/content.php
@@ -1,13 +1,13 @@
 
-<div class='cont block grid middle'>
-  <div class='tit block'>BOC</div>
-  <div class='lines'>
-  <hr><hr><hr>
-  </div>
-  <div class='lines'>
-  <hr><hr><hr>
-  </div>
-  <div class='lines'>
-  <hr><hr><hr>
-  </div>
-</div>
+    <div class='cont block grid middle'>
+      <div class='tit block'>BOC</div>
+      <div class='lines'>
+      <hr><hr><hr>
+      </div>
+      <div class='lines'>
+      <hr><hr><hr>
+      </div>
+      <div class='lines'>
+      <hr><hr><hr>
+      </div>
+    </div>
