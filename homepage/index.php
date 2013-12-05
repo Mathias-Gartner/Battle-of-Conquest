a7c34ff01f806db5b8515a23384f5badaf514018
Anh Tu <if12b022@technikum-wien.at>
2013-12-05 21:14:03 +0100

    Design Bereich

---
+++ homepage/index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<script type="text/javascript" src="js/jquery-2.0.2.min.js"></script>
+</head>
+<body>
+<?php
+//Banner
+include_once("pages/banner.php");
+?>
+
+<div id="content">
+<?php
+if (isset($_GET['p']))
+{
+	switch($_GET['p']) {
+	case "home":
+		include("pages/home.php");
+		break;
+	default:
+		include("pages/home.php");
+		break;
+	}
+} else
+{
+	include("pages/home.php");
+}
+?>
+
+</div>
+</body>
+</html>
---
+++ homepage/pages/banner.php
@@ -0,0 +1,15 @@
+
+	<div id="banner">
+		<!-- Logo -->
+		shoppingtour<span class="tk">.tk</span>
+		<!-- Menu -->
+		<div id="menu">
+			<ul class="links">
+				<li><a href="?p=home">Home</a></li>
+				<li><a href="?p=about">About</a></li>
+				<li><a href="?p=contact">Contact</a></li>
+			</ul>
+		</div>
+	</div>
+	
+<!-- End -->
