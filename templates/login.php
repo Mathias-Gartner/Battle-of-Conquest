4ae21463d81a4f67baf7825befc5816d5737f53a
Mathias Gartner <mathias.gartner@technikum-wien.at>
2013-12-12 00:23:05 +0100

    login page style fixed

--- templates/login.php
+++ templates/login.php
@@ -1,20 +1,22 @@
 <?php include "header.php" ?>
-<div id="loginBox">
-	<form action="index.php?action=login" method="POST">
-		<?php if (isset($PAGEDATA["loginError"]) && $PAGEDATA["loginError"]) { ?>
-		<div>
-			<span class="errorMessage">Login fehlgeschlagen. Benutzername oder Passwort ung&uuml;ltig.</span>
-		</div>
-		<?php } ?>
-		<div>
-			<input type="text" id="username" name="username" placeholder="Username" />
-		</div>
-		<div>
-			<input type="password" id="password" name="password" placeholder="Passwort" />
-		</div>
-		<div>
-			<input type="submit" id="login" name="login" value="Login" />
-		</div>
-	</form>
+<div class="cont shadow grid middle">
+  <div id="loginBox">
+	  <form action="index.php?action=login" method="POST">
+		  <?php if (isset($PAGEDATA["loginError"]) && $PAGEDATA["loginError"]) { ?>
+		  <div>
+			  <span class="errorMessage">Login fehlgeschlagen. Benutzername oder Passwort ung&uuml;ltig.</span>
+		  </div>
+		  <?php } ?>
+		  <div>
+			  <input type="text" id="username" name="username" placeholder="Username" />
+		  </div>
+		  <div>
+			  <input type="password" id="password" name="password" placeholder="Passwort" />
+		  </div>
+		  <div>
+			  <input type="submit" id="login" name="login" value="Login" />
+		  </div>
+	  </form>
+  </div>
 </div>
 <?php include "footer.php" ?>
