79afbfa46e6553866579c6f9b7244bfee46475a0
Markus Krammer <krammer.markus.92@gmail.com>
2013-12-13 18:20:15 +0100

    maptesting adding great canyons

---
+++ maptesting/map_script.js
@@ -0,0 +1,11 @@
+function init(){
+	$('.boc_mapster').mapster({
+		noHrefIsMask: false,
+        fillColor: 'EBEBEB',
+        fillOpacity: 0.3,
+        mapKey: 'data-group',
+		onClick: function (e) {
+			window.location.href = this.href;
+		}
+    });
+}
