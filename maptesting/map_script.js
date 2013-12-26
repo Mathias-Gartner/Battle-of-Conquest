597783dc8ad5a2ac53f1d6b6fae21231a85b3dca
SteaM92 <krammer.markus@hotmail.com>
2013-12-26 16:57:02 +0100

    attack cities

--- maptesting/map_script.js
+++ maptesting/map_script.js
@@ -5,13 +5,17 @@ function init(){
         fillOpacity: 0.3,
         mapKey: 'data-group',
 		onClick: function (e) {
-			window.location.href = this.href;
-		},
-		onMouseover: function (e) {
-			setShowBox(this);
+			if(this.id=="my_district"){
+				window.location.href = this.href;
+			}else{
+				setShowBox(this);
+			}
 		}
     });
 	setMyDistrict();
+	$('#boc_open_docks_id').click(function(event){
+		hideBox();
+	});
 }
 
 function setMyDistrict(){
@@ -24,6 +28,9 @@ function setShowBox(obj){
 	var y = position[1];
 	document.getElementById("box").style.top = y+"px";
 	document.getElementById("box").style.left = x+"px";
+	$('#attack_button').click(function(event){
+		window.location.href = "attack.php?id=" + obj.id;
+	});
 	showBox();
 }
 
