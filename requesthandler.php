7e629e86e82e59285b6cc9852b6486dc31478db4
sladiri <sladjanri@live.com>
2013-12-28 12:57:18 +0100

    modify requesthandler.php: Einrueckungen vergessen

--- requesthandler.php
+++ requesthandler.php
@@ -6,78 +6,78 @@ class RequestHandler
 	{
 		TORM\Log::enable(true);
   	
-  	$pageHandler = null;
-  	if (isset($_GET['action']) )
-  	{
-		switch ($_GET['action'])
+		$pageHandler = null;
+		if (isset($_GET['action']) )
 		{
-		case 'login':
-			$pageHandler = new PageHandlers\LoginPageHandler();
-			break;
-		case 'overview':
-			$pageHandler = new PageHandlers\OverviewPageHandler();
-			break;
-		case 'buildings':
-			$pageHandler = new Pagehandlers\BuildingsPageHandler();
-			break;
-		case 'units':
-			$pageHandler = new PageHandlers\UnitsPageHandler();
-			break;
-		case 'attacks':
-			$pageHandler = new PageHandlers\AttacksPageHandler();
-			break;
-		case 'map':
-			$pageHandler = new PageHandlers\MapPageHandler();
-			break;
+			switch ($_GET['action'])
+			{
+			case 'login':
+				$pageHandler = new PageHandlers\LoginPageHandler();
+				break;
+			case 'overview':
+				$pageHandler = new PageHandlers\OverviewPageHandler();
+				break;
+			case 'buildings':
+				$pageHandler = new Pagehandlers\BuildingsPageHandler();
+				break;
+			case 'units':
+				$pageHandler = new PageHandlers\UnitsPageHandler();
+				break;
+			case 'attacks':
+				$pageHandler = new PageHandlers\AttacksPageHandler();
+				break;
+			case 'map':
+				$pageHandler = new PageHandlers\MapPageHandler();
+				break;
+			}
 		}
-	}
-	else
-	{
-		$pageHandler = new PageHandlers\WelcomePageHandler();
-	}
+		else
+		{
+			$pageHandler = new PageHandlers\WelcomePageHandler();
+		}
 
-	if ($pageHandler != null)
-	{
-		if ($pageHandler->loginRequired() && !SessionUtility::isLoggedIn())
-			$pageHandler = new PageHandlers\LoginPageHandler();
+		if ($pageHandler != null)
+		{
+			if ($pageHandler->loginRequired() && !SessionUtility::isLoggedIn())
+				$pageHandler = new PageHandlers\LoginPageHandler();
 
-		$pageHandler = $pageHandler->handle();
-		$pageHandler->render();
-		exit(0);
+			$pageHandler = $pageHandler->handle();
+			$pageHandler->render();
+			exit(0);
+		}
+		
+		// orm samples
+		/* create object
+		$user = new Classes\User();
+		$user->setUsername('admin');
+		$user->setAge(1);
+		$user->mail('admin@localhost.com');
+		$user->setPassword('123456');
+		$user->save();
+		/**/
+		
+		/* load object
+		$user = Classes\User::find(1);
+		var_dump($user);
+		$collection = $user->districts;
+		while (($district = $collection->next()) != NULL)
+		  echo '<p>'.$district->getName().'</p>';
+		/**/
+		
+		/* create object with relation
+		$district = new Classes\District();
+		$district->setName('Entenhausen');
+		$district->setPosition(5, 10);
+		$district->setDistrictThreat(2);
+		$district->owner_id = 0; // 0 .. pk of user
+		$district->save();
+		/**/
+		
+		/* load object with relation
+		$district = Classes\District::first(array('district_name'=>'Entenhausen'));
+		var_dump($district->owner); // prints user
+		/**/
 	}
-  	
-  	// orm samples
-    /* create object
-    $user = new Classes\User();
-    $user->setUsername('admin');
-    $user->setAge(1);
-    $user->mail('admin@localhost.com');
-    $user->setPassword('123456');
-    $user->save();
-    /**/
-    
-    /* load object
-   	$user = Classes\User::find(1);
-   	var_dump($user);
-   	$collection = $user->districts;
-   	while (($district = $collection->next()) != NULL)
-   	  echo '<p>'.$district->getName().'</p>';
-   	/**/
-   	
-   	/* create object with relation
-   	$district = new Classes\District();
-   	$district->setName('Entenhausen');
-   	$district->setPosition(5, 10);
-   	$district->setDistrictThreat(2);
-   	$district->owner_id = 0; // 0 .. pk of user
-   	$district->save();
-   	/**/
-   	
-   	/* load object with relation
-   	$district = Classes\District::first(array('district_name'=>'Entenhausen'));
-   	var_dump($district->owner); // prints user
-   	/**/
-  }
 }
 
 ?>
