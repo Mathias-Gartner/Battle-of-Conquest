8ac48e4222ae1c12d063e8add15768ac47d20d52
sladiri <sladjanri@live.com>
2014-01-05 20:51:32 +0100

    refactor all CSS, HTML

--- templates/footer.php
+++ templates/footer.php
@@ -1,17 +1,9 @@
 
-   <div class="foot">
-   		<a href="?action=footer&foot=fh">
-   			<div class='grid centered'>FH Technikum</div>
-   		</a>
-   		<a href="?action=footer&foot=team">
-   			<div class='grid centered'>Team</div>
-   		</a>
-   		<a href="?action=footer&foot=help">
-   			<div class='grid centered'>Hilfe</div>
-   		</a>
-   		<a href="?action=footer&foot=imp">
-   			<div class='grid centered'>Impressum</div>
-   		</a>
+   <div class="foot outer centered">
+   		<a href="?action=footer&foot=fh" class="centered">FH Technikum</a>
+   		<a href="?action=footer&foot=team" class="centered">Team</a>
+   		<a href="?action=footer&foot=help" class="centered">Hilfe</a>
+   		<a href="?action=footer&foot=imp" class="centered">Impressum</a>
    </div>
   </body>
 </html>
--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html class='flex_vert'>
   <head>
     <link rel="stylesheet" type="text/css" href="res/main.css" />
     <link rel="stylesheet" type="text/css" href="res/overview.css" />
@@ -7,20 +7,20 @@
     <link rel="stylesheet" type="text/css" href="res/map.css" />
   </head>
   
-  <body class='site'>
-    <div class='nav outer' id="navi">
-      <a href="index.php"><div class=' grid centered'>Home</div></a>
-      <a href="?action=attacks"><div class=' grid centered'>Attacks</div></a>
-      <a href="?action=map"><div class=' grid centered'>Maps</div></a>
-      <a href="?action=buildings"><div class=' grid centered'>Buildings</div></a>
-      <a href="?action=units"><div class=' grid centered'>Units</div></a>
+  <body class='site text_style flex_vert'>
+    <div class='nav outer flex_hor'>
+      <a href="index.php"><div class=' grid flex_centered'>Home</div></a>
+      <a href="?action=attacks"><div class=' grid flex_centered'>Attacks</div></a>
+      <a href="?action=map"><div class=' grid flex_centered'>Maps</div></a>
+      <a href="?action=buildings"><div class=' grid flex_centered'>Buildings</div></a>
+      <a href="?action=units"><div class=' grid flex_centered'>Units</div></a>
       <?php if (isset($_SESSION['username'])) { ?>
-      <a href="?action=overview"><div class=' grid centered'>Overview</div></a>
+      <a href="?action=overview"><div class=' grid flex_centered'>Overview</div></a>
       <?php } else { ?>
-      <a href="?action=login"><div class=' grid centered'>Login</div></a>
+      <a href="?action=login"><div class=' grid flex_centered'>Login</div></a>
       <?php } ?>
     </div>
   
-  <div class='head grid outer centered logo'>
+  <div class='head grid middle centered logo'>
     <span class="logofont">battle of conquest</span>
   </div>
