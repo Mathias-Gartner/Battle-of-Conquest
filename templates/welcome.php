a3c32b9ed9db22a1e8773258fe299aa89a624ec1
Mathias Gartner <mathias.gartner@technikum-wien.at>
2014-01-21 00:49:26 +0100

    link fixed

--- templates/welcome.php
+++ templates/welcome.php
@@ -7,7 +7,7 @@
 <div class='flex_vert flex_centered' id='welcome'>
 	<p>BOC is short for "Battle of Conquest", a game that is being planed and produced by studends of FH Technikum in Vienna.</p>
 	<p>To begin with, BOC is a strategy-game and that means that it has to provide all the needs of these kind of games. Logical thinking and well timed actions are the keys to have success. Attacking without plans or timing will only hurt the attacker, in addition to that wasting all the ressources will not help the player in anyway. The army is one of the best things to help the player which means it has to be strong.</p>
-	<p>The main target-group are young people who have a weakness and love for strategy-games. More information about the game here: <a href="hilfe.html">link</a></p>
+	<p>The main target-group are young people who have a weakness and love for strategy-games. More information about the game here: <a href="?action=footer&foot=help">Help</a></p>
 </div>
 
 <?php include 'footer.php' ?>
