b03362a4e2658b9796928b5635792cc697d0f708
sladiri <sladjanri@live.com>
2014-01-20 16:55:52 +0100

    edit main.css, login.php: login form error msg position

--- templates/login.php
+++ templates/login.php
@@ -4,22 +4,14 @@
 
 <div id="loginBox" class="flex_centered">
 	<form action="index.php?action=login" method="POST" class="flex_vert flex_centered">
+		<div id="loginFields" class='flex_vert'>
+			<input type="text" id="username" name="username" placeholder="Username" required autofocus />
+			<input type="password" id="password" name="password" placeholder="Passwort" required />
+			<input type="submit" id="login" name="login" value="Login" />
+		</div>
 		<?php if (isset($PAGEDATA["loginError"]) && $PAGEDATA["loginError"]) { ?>
-		<div>
-			<span class="errorMessage">Login failed. Please check username and password.</span>
-		</div>
+		<span class="errorMessage">Login failed. Please check username and password.</span>
 		<?php } ?>
-		<div id="loginFields">
-			<div>
-				<input type="text" id="username" name="username" placeholder="Username" required autofocus />
-			</div>
-			<div>
-				<input type="password" id="password" name="password" placeholder="Passwort" required />
-			</div>
-			<div class='flex_centered'>
-				<input type="submit" id="login" name="login" value="Login" />
-			</div>
-		</div>
 	</form>
 </div>
 
