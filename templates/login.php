1616336091bd6f6cd0048009e358d70632a581d0
sladiri <sladjanri@live.com>
2014-01-20 17:04:26 +0100

    edit main.css, login.php: cleanup CSS

--- templates/login.php
+++ templates/login.php
@@ -2,16 +2,16 @@
 
 <div class='title flex_centered'>Please login.</div>
 
-<div id="loginBox" class="flex_centered">
+<div id="loginBox">
 	<form action="index.php?action=login" method="POST">
 		<div id="loginFields" class='flex_vert'>
 			<input type="text" id="username" name="username" placeholder="Username" required autofocus />
 			<input type="password" id="password" name="password" placeholder="Passwort" required />
 			<input type="submit" id="login" name="login" value="Login" />
+			<?php if (isset($PAGEDATA["loginError"]) && $PAGEDATA["loginError"]) { ?>
+				<span class="errorMessage">Login failed. Please check username and password.</span>
+			<?php } ?>
 		</div>
-		<?php if (isset($PAGEDATA["loginError"]) && $PAGEDATA["loginError"]) { ?>
-		<span class="errorMessage">Login failed. Please check username and password.</span>
-		<?php } ?>
 	</form>
 </div>
 
