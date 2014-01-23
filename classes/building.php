57c294e566658ffc62c3a6cc6c381c91ff1c3962
sladiri <sladjanri@live.com>
2014-01-23 17:14:21 +0100

    add missing Klammern, tsts ^^

--- classes/building.php
+++ classes/building.php
@@ -8,107 +8,107 @@ class Building extends \TORM\Model
 	{
 		return $this->get("building");
 	}
-	
+
 	public function setBuilding($building)
 	{
 		$this->set("building", $building);
 	}
-	
+
 	public function getResources()
 	{
 		return $this->get("resources");
 	}
-	
+
 	public function setResources($resources)
 	{
 		$this->set("resources", $resources);
 	}
-	
+
 	public function getMoral()
 	{
 		return $this->get("moral");
 	}
-	
+
 	public function setMoral($moral)
 	{
 		$this->set("moral", $moral);
 	}
-	
+
 	public function getPeople()
 	{
 		return $this->get("people");
 	}
-	
+
 	public function setPeople($people)
 	{
 		$this->set("people", $people);
 	}
-	
+
 	public function getDiseases()
 	{
 		return $this->get("diseases");
 	}
-	
+
 	public function setDiseases($diseases)
 	{
 		$this->set("diseases", $diseases);
 	}
-	
+
 	public function getLuck()
 	{
-		return $this->get("luck";
+		return $this->get("luck");
 	}
-	
+
 	public function setLuck($luck)
 	{
 		$this->set("luck", $luck);
 	}
-	
+
 	public function getUnitsAtk()
 	{
 		return $this->get("units_atk");
 	}
-	
+
 	public function setUnitsAtk($atk)
 	{
 		$this->set("units_atk", $atk);
 	}
-	
+
 	public function getUnitsDef()
 	{
 		return $this->get("units_def");
 	}
-	
+
 	public function setUnitsDef($def)
 	{
 		$this->set("units_def", $def);
 	}
-	
+
 	public function getMoveSpeed()
 	{
 		return $this->get("move_speed");
 	}
-	
+
 	public function setMoveSpeed($moveSpeed)
 	{
 		$this->set("move_speed", $moveSpeed);
 	}
-	
+
 	public function getBuildSpeed()
 	{
 		return $this->get("build_speed");
 	}
-	
+
 	public function setBuildSpeed($buildSpeed)
 	{
 		$this->set("build_speed", $buildSpeed);
 	}
-	
+
 	public function getResourceSpeed()
 	{
 		return $this->get("resource_speed");
 	}
-	
+
 	public function setResourceSpeed($resourceSpeed)
 	{
 		return $this->set("resource_speed", $resourceSpeed);
--- classes/distance.php
+++ classes/distance.php
@@ -9,22 +9,22 @@ class Distance extends \TORM\Model
 		$this->set("district_a", $districtA);
 		$this->set("district_b", $districtB);
 	}
-	
+
 	public function getTime()
 	{
 		return $this->get("time");
 	}
-	
+
 	public function setTime($time)
 	{
 		$this->set("time", $time);
 	}
-	
+
 	public function getDistance()
 	{
 		return $this->get("distance");
 	}
-	
+
 	public function setDistance($distance)
 	{
 		return $this->set("distance", $distance);
@@ -32,8 +32,8 @@ class Distance extends \TORM\Model
 }
 
 Distance::setPK("distance_id");
-Distance::belongsTo("districtA", array("class_name"=>"District", "primary_key"=>"district_id", "foreign_key"=>"district_a");
-Distance::belongsTo("districtB", array("class_name"=>"District", "primary_key"=>"district_id", "foreign_key"=>"district_b");
+Distance::belongsTo("districtA", array("class_name"=>"District", "primary_key"=>"district_id", "foreign_key"=>"district_a"));
+Distance::belongsTo("districtB", array("class_name"=>"District", "primary_key"=>"district_id", "foreign_key"=>"district_b"));
 Distance::validates("distance", array("numericality"=>true));
 
 ?>
