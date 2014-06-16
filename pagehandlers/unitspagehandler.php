03034a9a715e955ae034cbf18b62ee4ee00aaf15
Mathias Gartner <mathias.gartner@aon.at>
2014-06-16 12:02:45 +0200

    Units page
    - All available units are listed (like on PrepareAttack page)
    - Max 10 new units at a time (until we have resources)
    - Logic is prepared for resources

--- pagehandlers/unitspagehandler.php
+++ pagehandlers/unitspagehandler.php
@@ -8,49 +8,118 @@ class UnitsPageHandler extends PageHandler
 	{
 		if (isset($_GET['ajax']))
 		{
-		
+
 		}
 		$this->setPhpTemplate('units');
-		
-		if (isset($_GET['perform']) && $_GET['perform'] == 'debugAdd')
+
+		if (isset($_GET['method']) && $_GET['method'] == 'createUnits')
 		{
-			if (isset($_POST['city']))
-				$cityId = $_POST['city'];
-				
-			if (!is_numeric($cityId) || $cityId <= 0
-				|| \Classes\District::find($cityId) == null)
-			{
-				$this->setReturnCode(500);
-				echo 'cityId';
+			if (!$this->createUnits())
 				return $this;
+		}
+
+		$districtId;
+		if (isset($_GET['districtId']) && is_numeric($_GET['districtId']))
+		{
+			$districtId = $_GET['districtId'];
+		}
+		else
+		{
+			$districtId = $this->getMainDistrictForCurrentPlayer();
+			if ($districtId < 0)
+				return;
+		}
+
+		$district = \Classes\District::find($districtId);
+		if ($district->getOwnerId() != $_SESSION['userid'])
+		{
+			$this->setReturnCode(403);
+			$this->setMessage('You cannot create units in a district you don\'t own');
+			return $this;
+		}
+
+		$this->setPageData('districtId', $districtId);
+		$this->setPageData('cityName', $district->getName());
+
+		$builder = \Classes\Unit::makeBuilder();
+		$units = new \Torm\Collection($builder, null, '\Classes\Unit');
+
+		$unitData = array();
+		while (($unit = $units->next()) != NULL)
+		{
+			array_push($unitData, array('id'=>$unit->getUnitId(), 'name'=>$unit->getUnitName(), 'resources'=>$unit->getUnitResources(), 'max'=>10));
+			// TODO: getPlayersResources() and calculate max
+		}
+
+		$this->setPageData('units', $unitData);
+
+	  return $this;
+	}
+
+	private function createUnits()
+	{
+		if (isset($_POST['districtId']))
+			$districtId = $_POST['districtId'];
+
+		if (!is_numeric($districtId) || $districtId <= 0
+			|| \Classes\District::find($districtId) == null)
+		{
+			$this->setReturnCode(400);
+			$this->setMessage('districtId is missing');
+			return false;
+		}
+
+		\TORM\Connection::getConnection()->beginTransaction();
+		$resourceCost = 0;
+		$i = 1;
+		while (isset($_POST['unit_'.$i]) && is_numeric($_POST['unit_'.$i]))
+		{
+			$unit = \Classes\Unit::find($i);
+			if ($unit == null)
+			{
+				continue;
 			}
-			
-			$collection = \Classes\DistrictUnit::where(array('district_id'=>$cityId, 'unit_id'=>1));
+
+			$collection = \Classes\DistrictUnit::where(array('district_id'=>$districtId, 'unit_id'=>$unit->getUnitId()));
 			if ($collection->count() > 0)
 			{
-				$unit = $collection->next();
+				$districtUnit = $collection->next();
 			}
 			else
 			{
-				$unit = new \Classes\DistrictUnit();
-				$unit->setDistrictId($cityId);
-				$unit->setUnitId(1);
+				$districtUnit = new \Classes\DistrictUnit();
+				$districtUnit->setDistrictId($districtId);
+				$districtUnit->setUnitId($unit->getUnitId());
 			}
-			$unit->setCount($unit->getCount() + 10);
-			$unit->save();
-			
-			$this->setPageData('success', 1);
+			$districtUnit->setCount($districtUnit->getCount() + $_POST['unit_'.$i]);
+			$resourceCost += $unit->getUnitResources();
+			if (!$districtUnit->save())
+			{
+				\TORM\Connection::rollBack();
+				$this->setMessage("Failed to save unit");
+				$this->setReturnCode(500);
+				return false;
+			}
+
+			$i++;
 		}
-		
-		$districts = array();
-		$collection = \Classes\District::where(array('owner_id'=>$_SESSION['userid']));
-		while (($district = $collection->next()) != null)
+
+		//TODO: reduce resources for player; rollBack if he hasn't enough
+		\TORM\Connection::getConnection()->commit();
+		$this->setPageData('success', 1);
+		return true;
+	}
+
+	private function getMainDistrictForCurrentPlayer()
+	{
+		$district = \Classes\District::first(array('owner_id'=>$_SESSION['userid']));
+		if ($district == null)
 		{
-			array_push($districts, array('id'=>$district->getDistrictId(), 'name'=>$district->getName()));
+			$this->setReturnCode(500);
+			$this->setMessage("Current player has no district");
+			return -1;
 		}
-		$this->setPageData('cities', $districts);
-		
-	  return $this;
+		return $district->getDistrictId();
 	}
 }
 
--- templates/units.php
+++ templates/units.php
@@ -5,18 +5,27 @@
             <h2 class='flex_centered'>Build units</h2>
         </header>
 
-		<div>
-			<?php if (isset($PAGEDATA['success'])) { ?>
-			<span style="color: green; font-weight: bold;">10 Units added</span>
-			<?php } ?>
-			<form class='flex_vert' action='?action=units&perform=debugAdd' method='POST'>
-				<select name='city'>
-				<?php foreach ($PAGEDATA['cities'] as $city) { ?>
-					<option value='<?php echo $city['id']; ?>'><?php echo $city['name']; ?></option>
-				<?php } ?>
-				</select>
-				<input type='submit' class="bigbutton" value='Add 10 units' />
-			</form>
-		</div>
+    <?php if (isset($PAGEDATA['success'])) { ?>
+    <p><span style="color: green; font-weight: bold;">Your units have finished their training.</span><p>
+    <?php } ?>
+
+    <p><span class='quote'>How many soldiers shall we train, commander?</span></p>
+
+    <form class='flex_vert' action='?action=units&method=createUnits' method='POST'>
+      <input type='hidden' id='districtId' name='districtId' value='<?php echo $PAGEDATA['districtId']; ?>' />
+
+      <?php foreach ($PAGEDATA['units'] as $unit) { ?>
+      <p>
+        <span><?php echo $unit['name'].' ' ?>: </span>
+        <input type='number' class='unitNumber' onchange='unitNumberChanged()' name='unit_<?php echo $unit['id']; ?>' value='0' min='0' max='<?php echo $unit['max']; ?>' />
+        <span> of <?php echo $unit['max']; ?></span>
+      </p>
+      <?php } ?>
+
+      <p id='distanceParagraph' style='visibility: hidden;'>We will need <span id='resourcesRequiredLabel'></span> of our resources to train these men.</p>
+
+      <input type='submit' value='Begin your work!' />
+    </form>
+
 	</article>
 <?php include 'footer.php' ?>
